da01a72b79280c6fe26e153a99d4e302bf390899
Junyong Moon <fancy.junyongmoon@gmail.com>
2025-01-11 00:45:41 +0900

    init(devfancy): 페이먼츠 모듈 구성 (#8)
    - 도메인별로 모듈 구성
    - 디테일한 부분은 나중에 수정할 예정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,56 +1,65 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.4.1"
-    id("io.spring.dependency-management") version "1.1.7"
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
+    id("org.springframework.boot") version "3.4.1" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1" apply false
 }
 
-group = "tech.sipe"
-version = "0.0.1-SNAPSHOT"
+allprojects {
+    group = "com.smart"
+    version = "0.0.1-SNAPSHOT"
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+    repositories {
+        mavenCentral()
     }
 }
 
-repositories {
-    mavenCentral()
-}
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "io.spring.dependency-management")
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
     }
-}
 
-// scripts 경로의 pre-commit hook 등록
-tasks.register("addLintPreCommitHook", DefaultTask::class) {
-    group = "setup"
-    description = "Install git hooks"
-    doLast {
-        val hooksDir = project.file(".git/hooks")
-        val scriptDir = project.file("scripts")
-        val preCommit = scriptDir.resolve("pre-commit")
-        preCommit.copyTo(hooksDir.resolve("pre-commit"), overwrite = true)
-        hooksDir.resolve("pre-commit").setExecutable(true)
+    dependencies {
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        //implementation("org.jetbrains.kotlin:kotlin-reflect")
+        //testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+        //testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     }
-}
 
-// compileKotlin가 addGitPreCommitHook에 의존하도록 설정
-tasks.named("compileKotlin") {
-    dependsOn("addGitPreCommitHook")
-}
+//    // scripts 경로의 pre-commit hook 등록
+//    tasks.register("addLintPreCommitHook", DefaultTask::class) {
+//        group = "setup"
+//        description = "Install git hooks"
+//        doLast {
+//            val hooksDir = project.file(".git/hooks")
+//            val scriptDir = project.file("scripts")
+//            val preCommit = scriptDir.resolve("pre-commit")
+//            preCommit.copyTo(hooksDir.resolve("pre-commit"), overwrite = true)
+//            hooksDir.resolve("pre-commit").setExecutable(true)
+//        }
+//    }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs += listOf("-Xjsr305=strict")
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+//    // compileKotlin가 addGitPreCommitHook에 의존하도록 설정
+//    tasks.named("compileKotlin") {
+//        dependsOn("addLintPreCommitHook")
+//    }
 }
---
+++ transfer/src/test/kotlin/com/smart/payment/transfer/TransferApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.smart.payment.transfer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TransferApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
