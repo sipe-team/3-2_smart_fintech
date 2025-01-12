2d0bf9d40813f4b4a2b1b54b05a04ffcaf8bfd84
unanchoi <unanc2@gmail.com>
2025-01-12 14:34:41 +0900

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,17 +15,30 @@ java {
     }
 }
 
+extra["springModulithVersion"] = "1.3.1"
+
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.modulith:spring-modulith-starter-core")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.modulith:spring-modulith-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.modulith:spring-modulith-bom:${property("springModulithVersion")}")
+    }
+}
+
 
 kotlin {
     compilerOptions {
---
+++ src/main/kotlin/tech/sipe/fintech/account/Account.kt
@@ -0,0 +1,4 @@
+package tech.sipe.fintech.account
+
+class Account {
+}
