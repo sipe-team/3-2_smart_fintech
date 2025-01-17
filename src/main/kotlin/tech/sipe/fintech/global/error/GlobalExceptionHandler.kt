4760405ebd9018d48a36f1dc1bf073d1a90b482d
yb__char <uiurihappy@naver.com>
2025-01-17 16:24:02 +0900

    fix: GlobalExceptionHandler

--- src/main/kotlin/tech/sipe/fintech/global/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/tech/sipe/fintech/global/error/GlobalExceptionHandler.kt
@@ -1,6 +1,5 @@
 package tech.sipe.fintech.global.error
 
-import jakarta.validation.ConstraintViolation
 import jakarta.validation.ConstraintViolationException
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
@@ -13,11 +12,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import tech.sipe.fintech.common.logger
 import tech.sipe.fintech.global.common.GlobalResponse
-import java.util.function.Consumer
 
 @RestControllerAdvice(basePackages = ["tech.sipe.fintech"])
 class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
+	companion object {
+		private val log by logger()
+	}
+
 	override fun handleExceptionInternal(
 		ex: Exception,
 		body: Any?,
@@ -40,46 +43,28 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 		status: HttpStatusCode,
 		request: WebRequest,
 	): ResponseEntity<Any>? {
-		// log.error("MethodArgumentNotValidException : {}", e.message, e)
+		log.error("MethodArgumentNotValidException : {}", e.message, e)
 		val errorMessage: String? = e.bindingResult.allErrors[0].defaultMessage
 		val errorResponse =
 			ErrorResponse.of(e.javaClass.getSimpleName(), errorMessage!!)
 		val response: GlobalResponse = GlobalResponse.fail(status.value(), errorResponse)
-		return ResponseEntity.status(status).body<Any>(response)
+		return ResponseEntity.status(status).body(response)
 	}
 
 	/** Request Param Validation 예외 처리  */
 	@ExceptionHandler(ConstraintViolationException::class)
 	fun handleConstraintViolationException(e: ConstraintViolationException): ResponseEntity<GlobalResponse> {
-		// log.error("ConstraintViolationException : {}", e.message, e)
+		log.error("ConstraintViolationException: {}", e.message, e)
 
-		val bindingErrors: MutableMap<String?, Any> = HashMap()
-		e.constraintViolations
-			.forEach(
-				Consumer { constraintViolation: ConstraintViolation<*> ->
-					val propertyPath =
-						listOf(
-							*constraintViolation
-								.propertyPath
-								.toString()
-								.split("\\.".toRegex())
-								.dropLastWhile { it.isEmpty() }
-								.toTypedArray(),
-						)
-					val path =
-						propertyPath
-							.stream()
-							.skip(propertyPath.size - 1L)
-							.findFirst()
-							.orElse(null)
-					bindingErrors[path] = constraintViolation.message
-				},
-			)
+		val bindingErrors =
+			e.constraintViolations.associate { violation ->
+				val path = violation.propertyPath.toString().substringAfterLast(".", "unknown")
+				path to violation.message
+			}
 
-		val errorResponse =
-			ErrorResponse.of(e.javaClass.simpleName, bindingErrors.toString())
-		val response: GlobalResponse =
-			GlobalResponse.fail(HttpStatus.BAD_REQUEST.value(), errorResponse)
+		val errorResponse = ErrorResponse.of(e.javaClass.simpleName, bindingErrors.toString())
+		val response = GlobalResponse.fail(HttpStatus.BAD_REQUEST.value(), errorResponse)
+
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response)
 	}
 
@@ -88,7 +73,7 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 	protected fun handleMethodArgumentTypeMismatchException(
 		e: MethodArgumentTypeMismatchException,
 	): ResponseEntity<GlobalResponse> {
-		// log.error("MethodArgumentTypeMismatchException : {}", e.message, e)
+		log.error("MethodArgumentTypeMismatchException : {}", e.message, e)
 		val errorCode: ErrorCode = ErrorCode.METHOD_ARGUMENT_TYPE_MISMATCH
 		val errorResponse =
 			ErrorResponse.of(e.javaClass.getSimpleName(), errorCode.message)
@@ -104,7 +89,7 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 		status: HttpStatusCode,
 		request: WebRequest,
 	): ResponseEntity<Any>? {
-		// log.error("HttpRequestMethodNotSupportedException : {}", e.message, e)
+		log.error("HttpRequestMethodNotSupportedException : {}", e.message, e)
 		val errorCode: ErrorCode = ErrorCode.METHOD_NOT_ALLOWED
 		val errorResponse =
 			ErrorResponse.of(e.javaClass.getSimpleName(), errorCode.message)
@@ -116,7 +101,7 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 	/** CustomException 예외 처리  */
 	@ExceptionHandler(CustomException::class)
 	fun handleCustomException(e: CustomException): ResponseEntity<GlobalResponse> {
-		// log.error("CustomException : {}", e.message, e)
+		log.error("CustomException : {}", e.message, e)
 		val errorCode: ErrorCode = e.errorCode
 		val errorResponse =
 			ErrorResponse.of(errorCode.name, errorCode.message)
@@ -128,7 +113,7 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 	/** 500번대 에러 처리  */
 	@ExceptionHandler(Exception::class)
 	protected fun handleException(e: Exception): ResponseEntity<GlobalResponse> {
-		// log.error("Internal Server Error : {}", e.message, e)
+		log.error("Internal Server Error : {}", e.message, e)
 		val internalServerError: ErrorCode = ErrorCode.INTERNAL_SERVER_ERROR
 		val errorResponse =
 			ErrorResponse.of(e.javaClass.simpleName, internalServerError.message)
@@ -139,7 +124,7 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 
 	@ExceptionHandler(RuntimeException::class)
 	protected fun handleRuntimeException(e: RuntimeException): ResponseEntity<GlobalResponse> {
-		// log.error("Internal Server Error : {}", e.message, e)
+		log.error("Internal Server Runtime Error : {}", e.message, e)
 		val internalServerError: ErrorCode = ErrorCode.INTERNAL_SERVER_ERROR
 		val errorResponse =
 			ErrorResponse.of(e.javaClass.simpleName, internalServerError.message)
