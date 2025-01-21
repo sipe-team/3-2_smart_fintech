72e7b7b84b509c4ffde77793c3691c671d3fe7a1
ybchar <uiurihappy@naver.com>
2025-01-21 22:49:37 +0900

    comment

--- src/main/kotlin/tech/sipe/fintech/transfer/internal/domain/TransferType.kt
+++ src/main/kotlin/tech/sipe/fintech/transfer/internal/domain/TransferType.kt
@@ -1,7 +1,7 @@
 package tech.sipe.fintech.transfer.internal.domain
 
 enum class TransferType {
-    ACCOUNT_TO_WALLET,  // 계좌 → 지갑
-    WALLET_TO_ACCOUNT,  // 지갑 → 계좌
-    WALLET_TO_WALLET    // 지갑 → 지갑
+	ACCOUNT_TO_WALLET,
+	WALLET_TO_ACCOUNT,
+	WALLET_TO_WALLET,
 }
