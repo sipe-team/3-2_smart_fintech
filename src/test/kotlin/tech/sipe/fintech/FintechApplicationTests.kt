f4497bd00ddf2ab04fa0008b2e4623c40ee2086c
devifancy <fancy.junyongmoon@gmail.com>
2025-01-24 18:46:10 +0900

    feat: UML 다이어그램 그려주는 의존성 추가

--- src/test/kotlin/tech/sipe/fintech/FintechApplicationTests.kt
+++ src/test/kotlin/tech/sipe/fintech/FintechApplicationTests.kt
@@ -2,6 +2,7 @@ package tech.sipe.fintech
 
 import org.junit.jupiter.api.Test
 import org.springframework.modulith.core.ApplicationModules
+import org.springframework.modulith.docs.Documenter
 import org.springframework.modulith.test.ApplicationModuleTest
 
 @ApplicationModuleTest
@@ -13,5 +14,21 @@ class FintechApplicationTests {
 		println("modules: $modules")
 
 		modules.verify()
+	}
+
+	@Test
+	fun wri2teDocumentationSnippets() {
+		Documenter(modules)
+			.writeModulesAsPlantUml(
+// 				DiagramOptions.defaults()
+// 					.withStyle(DiagramOptions.DiagramStyle.UML)
+// 					.withDependencyTypes(DependencyType.USES_COMPONENT)
+			).writeIndividualModulesAsPlantUml()
+			.writeAggregatingDocument()
+// 			.writeModulesAsPlantUml(
+// 				DiagramOptions.defaults()
+// 					.withStyle(DiagramOptions.DiagramStyle.UML)
+// 					.withDependencyTypes(DependencyType.USES_COMPONENT)
+// 			)
 	}
 }
