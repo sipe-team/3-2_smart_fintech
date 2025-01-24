bbb492feb126d4bdafdbd95c802fc45236f55ba5
조상준 <juncho12011201@gmail.com>
2025-01-24 16:09:48 +0900

    refactor: 불필요한 null 검사 제거

--- src/main/kotlin/tech/sipe/fintech/wallet/presentation/CreateWalletRequest.kt
+++ src/main/kotlin/tech/sipe/fintech/wallet/presentation/CreateWalletRequest.kt
@@ -1,8 +1,5 @@
 package tech.sipe.fintech.wallet.presentation
 
-import jakarta.validation.constraints.NotNull
-
 data class CreateWalletRequest(
-	@field:NotNull(message = "User ID cannot be null")
 	val userId: Long,
 )
