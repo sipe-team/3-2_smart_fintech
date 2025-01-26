09c0afdb9df9537a7d51ee0aaeabfcf14ae89090
devifancy <fancy.junyongmoon@gmail.com>
2025-01-26 15:14:30 +0900

    fix: global 패키지의 ApplicationModule Type를 OPEN 으로 수정

--- src/main/kotlin/tech/sipe/fintech/global/GlobalModuleMetadata.kt
+++ src/main/kotlin/tech/sipe/fintech/global/GlobalModuleMetadata.kt
@@ -2,8 +2,13 @@ package tech.sipe.fintech.global
 
 import org.springframework.modulith.ApplicationModule
 
+/**
+ * global 패키지의 ApplicationModule type 이 CLOSED 로 설정되어 있어서 테스트가 실패가 됨
+ * ( CLOSED 의 경우해당 모듈 최상위 파일에만 접근 할 수 있어서 하위의 error 패키지는 접근 할 수 없습니다)
+ * OPEN 으로 수정하면 테스트가 정상적으로 통과됨
+ */
 @ApplicationModule(
-	type = ApplicationModule.Type.CLOSED,
+	type = ApplicationModule.Type.OPEN,
 	allowedDependencies = [],
 )
 class GlobalModuleMetadata
