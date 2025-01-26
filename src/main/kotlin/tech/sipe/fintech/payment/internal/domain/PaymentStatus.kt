b0aaedf303f3706f51e36cdbc6ba0a69ff8c22bd
devifancy <fancy.junyongmoon@gmail.com>
2025-01-26 16:28:42 +0900

    refactor: 케이스 상태 수정 (송금과 유사하게)

--- src/main/kotlin/tech/sipe/fintech/payment/internal/domain/PaymentStatus.kt
+++ src/main/kotlin/tech/sipe/fintech/payment/internal/domain/PaymentStatus.kt
@@ -1,7 +1,14 @@
 package tech.sipe.fintech.payment.internal.domain
 
 enum class PaymentStatus {
-	WAITING,
+	/**
+	 * 요청 (REQUESTED)
+	 * 취소 (CANCELED)
+	 * 실패 (FAILED)
+	 * 성공 (SUCCESS)
+	 */
+	REQUESTED,
+	CANCELED,
+	FAILED,
 	SUCCESS,
-	FAIL,
 }
